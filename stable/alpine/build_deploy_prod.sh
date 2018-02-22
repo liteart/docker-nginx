ea7d505a46bf0768edc2993c8aa7957285085e6c
Simon Hugentobler <simon.hugentobler@bertschi.com>
2018-02-22 11:05:12 +0100

    Added Deploy Scripts

---
+++ stable/alpine/build_deploy_prod.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+DATE=`date +%Y-%m-%d`
+docker build -t extranet.bertschi.com:5000/bertschi-nginx-spnego:latest -t extranet.bertschi.com:5000/bertschi-nginx-spnego:$DATE .
+docker login --username=controller --password=duesr05 --email=simon.hugentobler@bertschi.com extranet.bertschi.com:5000
+docker push extranet.bertschi.com:5000/bertschi-nginx-spnego:latest
+docker push extranet.bertschi.com:5000/bertschi-nginx-spnego:$DATE
+echo "created Version $DATE" >> ../docker-versions.log
+
+date
